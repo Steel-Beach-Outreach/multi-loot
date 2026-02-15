164ede55160b529e11d3f666c2a04287a2d082c2
Duke Manchester <73570126+dukeman74@users.noreply.github.com>
2026-02-15 16:48:58 -0700

    working item background and rotatable stack text

--- client/item_representation.gd
+++ client/item_representation.gd
@@ -1,7 +1,7 @@
 class_name ItemRepresentation extends TextureRect
 
-#var item_texture: TextureRect:
-#	get: return $ItemTexture
+var background_panel: Panel:
+	get: return $Panel
 
 var stack_label: Label:
 	get: return $StackLabel
@@ -15,11 +15,10 @@ var item:Item = null:
 		texture=item.sprite
 		update_rotation.call_deferred()
 		item.stack_updated.connect(update_stack)
+		background_panel.add_theme_stylebox_override(&"panel", item.definition.background_style_box)
 		update_stack()
 
 func update_stack() -> void:
-	stack_label.text = str(item.stack_size)
-	stack_label.visible = item.stack_size != 1
 	queue_redraw()
 
 func update_rotation() -> void:
@@ -47,9 +46,17 @@ func double_click() -> void:
 		
 const DEFAULT_FONT = preload("uid://dnevlpicxings")
 
+const stack_text_offset_from_bottom = 4
+
 func _draw() -> void:
+	if item.stack_size == 1: return
 	draw_set_transform(Vector2.ZERO,-rotation)
 	var my_top_left := Vector2.ZERO
 	if rotation != 0: my_top_left.x -= size.y
 	var my_bottom_right := my_top_left + (size if rotation == 0 else ItemContainer.transposed_vector2(size))
-	draw_string(DEFAULT_FONT, my_bottom_right, "wolno",HORIZONTAL_ALIGNMENT_CENTER,  100)
+	var my_bottom_left := Vector2(my_top_left.x, my_bottom_right.y)
+	var draw_position:Vector2 = my_bottom_left-Vector2(0,stack_text_offset_from_bottom)
+	var text:String =  str(item.stack_size)
+	var width:float = my_bottom_right.x-my_bottom_left.x
+	draw_string_outline(DEFAULT_FONT, draw_position,text,HORIZONTAL_ALIGNMENT_RIGHT,width,16,6,Color.BLACK)
+	draw_string(DEFAULT_FONT, draw_position, text,HORIZONTAL_ALIGNMENT_RIGHT, width)
--- model/item_definition.gd
+++ model/item_definition.gd
@@ -1,6 +1,7 @@
 class_name ItemDefinition extends Resource
 
 @export var sprite:Texture2D
+@export var background_style_box:StyleBox = preload("uid://cdm0ambin1yb")
 @export var weight_per:float
 @export var stack_max:int = 1
 @export var dimensions:Vector2i = Vector2i(1,1)
